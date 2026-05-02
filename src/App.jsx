24aae590feb2741d438f1207f84aa69f1cfda022
Avinash Malhotra <avimalhotra505@gmail.com>
2026-05-02 12:31:20 +0530

    loops and conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Greet from "./Greets";
 
 export default function App(){
 
     const cars=["swift","baleno","dzire","brezza","grand vitara"];
-    const swift={name:"swift", power:82, torque:112, price:800000};
+
+    const car={name:"swift", power:82, torque:112, price:800000};
+    
     const data=[
                 {"id":1,"name": "swift", "type": "hatchback", "price":850000},
                 {"id":2,"name": "dzire", "type": "sedan", "price":980000},
@@ -19,14 +22,13 @@ export default function App(){
                 {"id":11,"name": "wagon r", "type": "hatchback", "price":500000},
                 {"id":12,"name": "jimny", "type": "suv", "price":1400000}
             ];
-        // const hatchback=data.filter(i=>i.type=="hatchback");
-         
+
+            const hatch=data.filter(i=>i.type=="hatchback");
 
             const numbers=[0,1,2,3,4,5,6,7,8,9];
             const even=numbers.filter(i=>i%2==0);
-            const sum=numbers.reduce((x,y)=>x+y,0);
-            
-
+            const sum=numbers.reduce((x,y)=>x+y,0 );
+          
 
   return (
     <div className="container">
@@ -35,52 +37,52 @@ export default function App(){
 
         <main className="p-3">
           <h2>Hello User </h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eveniet impedit accusantium libero a incidunt quos deserunt nam repellendus rem ratione nulla alias sapiente beatae autem aliquam repellat, dicta aspernatur.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
-          <h3>Array</h3>
-          <ol>
-              {
-                cars.map((elem,ind)=>(
-                  <li key={ind}>{elem}</li> 
-                ))
-              }
-          </ol>
-          <hr />
-          <h3>Object</h3>
-          <table className="table table-bordered border-primary">
-            <thead><tr><th>Property</th><th>Value</th></tr></thead>
-            <tbody>
-              {
-                Object.entries(swift).map(([x,y],ind)=>(
-                  <tr key={ind}><td>{x}</td><td>{y}</td></tr>
-                ))
-              }
-            </tbody>
-          </table>
-          <hr />
-          <h3>JSON Data</h3>
-          <table className="table table-bordered border-primary">
-            <thead><tr><th>S No</th><th>Name</th><th>Body</th><th>Price</th><th>On Road</th></tr></thead>
-            <tbody>
-              {
-                data.map(elem=>(
-                  <tr key={elem.id}><td>{elem.id}</td><td>{elem.name}</td><td>{elem.type}</td><td>{elem.price}</td><td>{(elem.price*1.15).toFixed(0)}</td></tr>
-                ))
-              }
-            </tbody>
-           </table> 
-
-           <hr />
-
-           {/* <ol>
+            <hr />
+            <h2>Array</h2>
+            <ol>
             {
-              numbers.map((elem,ind)=>(
+            cars.map((elem,ind)=>(
                 <li key={ind}>{elem}</li>
+            ))
+            }
+            </ol>
+            <hr />
+            <h2>Object</h2>
+            <table className="table table-bordered"><tbody>
+            {
+              Object.entries(car).map(([key,val],ind)=>(
+                <tr key={ind}><td>{key}</td><td>{val}</td></tr>
               ))
             }
-           </ol> */}
+            </tbody>
+            </table>
+            <hr />
+            <h2>JSON Data</h2>
+              <table className="table table-bordered">
+              <thead>
+                  <tr className="table-dark">
+                    <th>S No</th>
+                    <th>Name</th>
+                    <th>Body Type</th>
+                    <th>Price</th>
+                  </tr>
+              </thead>  
+              <tbody>
+            {
+              data.map((elem)=>(
+                <tr key={elem.id}><td>{elem.id}</td><td>{elem.name}</td><td>{elem.type}</td><td>{elem.price.toLocaleString('en-in')}</td></tr>
+              ))
+            }
+            </tbody></table>
 
-        
+            <hr />
+
+            <Greet x={1}></Greet>
+          
+
+                
         </main>
         <Footer />
     </div>
---
+++ src/Greets.jsx
@@ -0,0 +1,12 @@
+export default function Greet({x}){
+     
+     // if(x){
+     //      return <h1>{x}</h1>
+     // }
+     // else{
+     //      return <p>x is not defined</p>
+     // }
+
+     return (x) ? "yes" : "no";
+
+}
