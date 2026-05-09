55283668443f0a13f8ea40241e174cba15e43be4
Avinash Malhotra <avimalhotra505@gmail.com>
2026-05-09 12:28:23 +0530

    useState in arrays and events

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ export default function App(){
   const [name,setName]=useState("");
   const [age,setAge]=useState(0);
   const [form,setForm]=useState({username:"", password:"", phone:""});
+  const [arr,setArr]=useState([]);
 
   function handleClick(){
       // setCounter(counter+1);
@@ -23,12 +24,26 @@ export default function App(){
     const {name,value}=e.target;
 
     setForm(prev=>(
-      {...prev, [name] : value }
+      { ...prev, [name] : value }
     ));
-
-
   }
 
+  /* array */
+
+  function addToArray(e){
+      e.preventDefault();
+      const x=e.target.num.value;
+      setArr([...arr,x]);
+      // document.querySelector("#num").value="";
+  }
+  function removeFromArray(e){
+      const t=e.target.parentElement.textContent.trim();
+      setArr(arr.filter(i=>i!=t));
+  }
+
+  function sayHello(x=""){
+    console.log(`datetime: ${x}`);
+  }
 
   return (
     <div className="container">
@@ -37,6 +52,10 @@ export default function App(){
 
         <main className="p-3">
           <h2>Hello User </h2>
+          
+          {/* <button onClick={sayHello} className="btn btn-secondary">Hello</button> */}
+          <button onClick={()=>sayHello(new Date().toLocaleString())} className="btn btn-secondary">Hello</button>
+
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
           <button className="btn btn-primary me-3" onClick={()=>setCounter(counter+1)}>Increment</button> 
@@ -59,9 +78,7 @@ export default function App(){
           <p>Name: {name}</p>
           <p>Age: {age}</p>
 
-
           <hr />
-
 
           <form action="" className="row align-items-center">
                 <div className="col-auto"><label className="form-label" htmlFor="username">Username</label></div>
@@ -72,6 +89,22 @@ export default function App(){
                 <div className="col-auto"><input type="password" name="password" id="password" value={form.password} className="form-control" onChange={handleChange}  /></div>
           </form>
           <p>Userame: {form.username}, 📞 : {form.phone}, Password: {form.password},  </p>
+          <hr />
+
+          <form className="row align-items-center" onSubmit={addToArray}>
+            <div className="col-auto"><label htmlFor="num" className="form-label">Add Name : </label></div>
+            <div className="col-auto"><input className="form-control" id="num" type="text" name="num" required /></div>
+            <div className="col-auto"><button className="btn btn-primary">Add</button></div>
+          </form>
+          <ol className="list-group">
+            {
+              arr.map((elem,ind)=>(
+                  <li className="list-group-item" key={ind}>{elem} 
+                  <button className="btn-close float-end" onClick={removeFromArray}></button>
+                  </li>
+              ))
+            }
+          </ol>
           
 
 
